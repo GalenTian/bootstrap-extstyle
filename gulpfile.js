f406ee7cc312345cfcd392b04d7dd102b020dff5
Galen Tian <galen_tian@126.com>
2017-07-07 14:55:09 +0800

    Add navbar-side style

--- gulpfile.js
+++ gulpfile.js
@@ -19,21 +19,21 @@ var mainFile = './themes/bootstrap/build.less',
     projectName = 'bootstrap-extstyle',
 
     distFolder = './dist/',
-    
+
     lessExt = '.less',
     cssExt = '.css',
     minCssExt = '.min.css',
-    
+
     allFiles = '*.*';
 
-var tasks = ['clean', 'css', 'css1', 'css2'];
+var tasks = ['clean', 'css', 'css1', 'css2', 'css3', 'copyFonts'];
 
 gulp.task(tasks[0], function () {
     return gulp.src(distFolder, { read: false }).pipe(clean());
 });
 
 gulp.task(tasks[1], [tasks[0]], function () {
-    
+
     return gulp.src(mainFile)
         .pipe(less())
         .pipe(header(banner, bannerJson))
@@ -46,7 +46,7 @@ gulp.task(tasks[1], [tasks[0]], function () {
 });
 
 gulp.task(tasks[2], [tasks[0]], function () {
-    
+
     return gulp.src('./themes/blue/build.less')
         .pipe(less())
         .pipe(header(banner, bannerJson))
@@ -59,7 +59,7 @@ gulp.task(tasks[2], [tasks[0]], function () {
 });
 
 gulp.task(tasks[3], [tasks[0]], function () {
-    
+
     return gulp.src('./themes/blue-glass/build.less')
         .pipe(less())
         .pipe(header(banner, bannerJson))
@@ -69,6 +69,28 @@ gulp.task(tasks[3], [tasks[0]], function () {
         .pipe(cleancss())
         //.pipe(header(banner, bannerJson))
         .pipe(gulp.dest(distFolder + 'blue-glass/css/'));
+});
+
+gulp.task(tasks[4], [tasks[0]], function () {
+
+    return gulp.src('./themes/dark/build.less')
+        .pipe(less())
+        .pipe(header(banner, bannerJson))
+        .pipe(rename(projectName + cssExt))
+        .pipe(gulp.dest(distFolder + 'dark/css/'))
+        .pipe(rename(projectName + minCssExt))
+        .pipe(cleancss())
+        //.pipe(header(banner, bannerJson))
+        .pipe(gulp.dest(distFolder + 'dark/css/'));
+});
+
+gulp.task(tasks[5], [tasks[1], tasks[2], tasks[3], tasks[4]], function () {
+
+  return gulp.src('./fonts/*')
+    .pipe(gulp.dest(distFolder + 'blue/fonts'))
+    .pipe(gulp.dest(distFolder + 'blue-glass/fonts'))
+    .pipe(gulp.dest(distFolder + 'dark/fonts'))
+    .pipe(gulp.dest(distFolder + 'bootstrap/fonts'));
 });
 
 gulp.task('default', tasks, function () {
